753183f3f410c85dc80d7224e1b1effbea19516e
Rodrigo Cordeiro <nile.cordeiro@gmail.com>
2025-01-27 17:22:36 -0300

    Novas features

--- 04-pizza-shop-web/src/App.tsx
+++ 04-pizza-shop-web/src/App.tsx
@@ -5,15 +5,19 @@ import { router } from './routes'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { Toaster } from 'sonner'
 import { ThemeProvider } from './components/theme/theme-provider'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
 
 export function App() {
  
   return (
     <HelmetProvider>
       <ThemeProvider storageKey='pizzashop-theme' defaultTheme='dark'>
-        <Helmet titleTemplate='%s | pizza.shooops'/>
+        <Helmet titleTemplate='%s | pizza.shops'/>
         <Toaster richColors />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>  
       </ThemeProvider>
     </HelmetProvider>
     
